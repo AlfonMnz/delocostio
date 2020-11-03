7915d11ef3b4295e10091b942cde6e0803562551
AlfonMnz <alfonmusan14@gmail.com>
2020-11-03 12:41:29 +0100

    changing words

--- src/response.js
+++ src/response.js
@@ -17,7 +17,7 @@ module.exports = {
         'Te has vuelto totalmente loco',
         'Estamos locos?',
         'Madre mía que LOCO estás',
-        'D E   L O W C O S T'
+        'D E   L O U C O S'
 
     ],
     accounts: [
