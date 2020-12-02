4cdd4022a9a91de8375cefcb9f3881174b4eb464
AlfonMnz <alfonmusan14@gmail.com>
2020-12-02 17:42:11 +0100

    end

---
+++ README.md
@@ -0,0 +1,10 @@
+# De Locos Tío 
+
+Bot de Twitter que responde automáticamente cada vez que una de las cuentas ponga un tweet con un mensaje aleatorio de la lista de repositorios
+
+## Como se usa
+1. Clona el repositorio
+2. Obtener una API KEY de la api de Twitter
+3. Copiar el archivo **_.env.example_**, llámalo **_.env_** y rellénalo con los datos de tu clave de Twitter.
+4. Rellena el archivo **_response.js_** con las ids de las cuentas a las que quieres que el bot responda. (Puedes obtener la ID de una cuenta de twitter [aquí](http://gettwitterid.com/)) y con las respuestas que quieras.
+5. Ten un feliz día :D
--- src/response.js
+++ src/response.js
@@ -1,29 +1,13 @@
 module.exports = {
     responses: [
-        'Nah, de locos',
-        'Madre mía estás loco tío',
-        'D E   L O C O S',
-        'Que tiemble MarkiLokuras que ha llegado un loco de verdad',
-        'Tío, como te pasas, que locura',
-        'Necesitamos al 112 por aquí, se ha escapado un loco',
-        'Por favor, para ya, estás loco',
-        'QUE ALGUIEN PARE A ESTE HOMBRE ESTÁ LOCO',
-        'Es un puto loco',
-        'Simplemente de locos',
-        'Nah, estás de la putísima olla',
-        'Pero qué coño haces puto loco?',
-        'Te has vuelto totalmente loco',
-        'Estamos locos o qué pasa?',
-        'Te has vuelto totalmente loco',
-        'Estamos locos?',
-        'Madre mía que LOCO estás',
-        'D E   L O U C O S'
+        /**
+         * Insert here your reply
+         */
 
     ],
     accounts: [
-        '2754746065',
-        '1484784811',
-        '145775695',
-        '274708552'
+        /**
+         * Insert here your accounts
+         */
     ]
 }
